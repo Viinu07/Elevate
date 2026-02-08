fbf8849f2598407f478209bb6c26cfecda195d40
Viinu_07 <viinu07@gmail.com>
2026-02-08 09:44:05 +0530

    feat: Implement profile page with user overview, award display, and peer nomination functionality.

--- src/modules/profile/ProfilePage.tsx
+++ src/modules/profile/ProfilePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import type { RootState } from '../../store';
 import { voteForAward, AWARD_CATEGORIES } from '../../store/collabSlice';
